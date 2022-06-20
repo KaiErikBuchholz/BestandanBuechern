2ede28f510a282960a8cb7c43e057e677701526c
buchhoka <kaibuchholz13@gmail.com>
2022-06-20 09:08:17 +0200

    inicial

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-class Buecher{
+class Buch {
+    constructor(Farbe, Verlag, Autor, Preis, Anzahl) {
+        this.Farbe = "Farbe"
+        this.Verlag = "Verlag"
+        this.Autor = "Autor"
+        this.Preis = "Preis"
+        this.Anzahl = "Anzahl"
+    }
 
-verfuegbar = false
-autor
-seiten
-verlag
-bestand
-
-constructor(autor, seiten, verlag, bestand) {
-    this.autor = schriftsteller
-    
+    verfuegbar() {
+        if (this.Anzahl < 1) {
+            console.log("Es ist kein Buch mehr vorhanden")
+        } else {
+            console.log("Es ist noch mindestens 1 Buch vorhanden")
+        }
+    }
+    verkauft() {
+        this.Anzahl = this.Anzahl - 1
+    }
+}
+function neuesFahrzeug() {
+    buch = new buch ("", "", "")
 }
 
+let lager = []
+
+function init() {
+    let buch = new Buch("Schwarz", "Duden", "J.K. Rowling", "19.99€")
+    lager.push(buch)
+    buch = new Buch("Weiß", "Cornelsen", "", "")
+    lager.push(buch)
+    buch = new Buch("Orange", "", "", "")
+    lager.push(buch)
+    buch = new Buch("Gelb", "", "", "")
+    lager.push(buch)
 }
